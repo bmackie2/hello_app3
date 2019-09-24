db989a1dd36442e01d69a2b9f4261e61841513ee
bmackie2 <bmackie2@its.jnj.com>
2019-09-24 17:21:29 +0000

    Udpate controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,17 +6,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-  def create
-    @user = User.new(user_params)
-      if @user.save
-        redirect_to '/'
-      end
-    end
-
-  private
-  def user_params
-      params.require(:user).permit(:name, :school)
-  end
 
 
 end
